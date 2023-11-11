c1b316436590c0759ea0401597013cb9205d8c53
Mx.Chetty <Le1727424@crcmail.net>
2023-11-11 15:26:20 -0500

    Added editor view to button

--- src/main/java/com/champlain/oop2/oop2assignement4/EnclosureDisplayViewController.java
+++ src/main/java/com/champlain/oop2/oop2assignement4/EnclosureDisplayViewController.java
@@ -1,10 +1,17 @@
 package com.champlain.oop2.oop2assignement4;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.util.List;
 
 public class EnclosureDisplayViewController {
@@ -22,8 +29,20 @@ public class EnclosureDisplayViewController {
 
 
     @FXML
-    protected void onEnclosureViewEditButtonClick() {
-        System.exit(0);
+    protected void onEnclosureViewEditButtonClick(ActionEvent pEvent) throws IOException {
+        Enclosure pEnclosure = new Enclosure();
+
+        FXMLLoader fxmlLoader = new FXMLLoader(ZooApplication.class.getResource("AnimalEditorView.fxml"));
+        Parent view = fxmlLoader.load();
+        AnimalEditorViewController newAnimalEditorViewController = fxmlLoader.getController();
+        newAnimalEditorViewController.setEnclosure();
+        Scene nextScene = new Scene(view, 500, 500);
+        Stage nextStage = new Stage();
+        nextStage.setScene(nextScene);
+        nextStage.setTitle(pEnclosure.getName());
+        nextStage.initModality(Modality.WINDOW_MODAL);
+        nextStage.initOwner(((Node) pEvent.getSource()).getScene().getWindow());
+        nextStage.showAndWait();
     }
     @FXML
     protected void onEnclosureAddButtonClick() {
